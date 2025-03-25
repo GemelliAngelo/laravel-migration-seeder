ec0cf633154bc5417e3c84d10d3d1ce340089fe6
GemelliAngelo <gemeangelo27@gmail.com>
2025-03-25 21:19:13 +0100

    fix bootstrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Migration Seeders</title>
+
+    @vite(["resources/sass/app.scss","resources/js/app.js"])
+
 </head>
 <body>
     <div class="container">
