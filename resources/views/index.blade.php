78ca1bd86914040c71dc3fa2d268451c9f609f99
GemelliAngelo <gemeangelo27@gmail.com>
2025-03-25 20:44:55 +0100

    print trains on page with model sql

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,42 @@
-<h1>HELLO WORLD!</h1>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Migration Seeders</title>
+</head>
+<body>
+    <div class="container">
+        <h1>HELLO WORLD!</h1>
+
+        @foreach ($trains as $train)
+            <ul>
+                <li>{{$train["company"]}}</li>
+                <li>{{$train["station_of_departure"]}}</li>
+                <li>{{$train["station_of_arrival"]}}</li>
+                <li>{{$train["departure_time"]}}</li>
+                <li>{{$train["arrival_time"]}}</li>
+                <li>{{$train["train_code"]}}</li>
+                <li>{{$train["number_of_carriage"]}}</li>
+                    @if (!$train["on_time"])
+                        <li>
+                            In Ritardo
+                        </li>
+
+                    @else
+                        <li>
+                            In Orario
+                        </li>
+                    @endif
+                    @if ($train["is_deleted"])
+                        <li>
+                            Cancellato
+                        </li>
+                    @endif
+            </ul>    
+        @endforeach
+    </div>
+    
+</body>
+</html>
